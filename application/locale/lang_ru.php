63c73caaa60a440356ee26e2d850ba60e5b02203
Andrey Kuts <akuts@softserveinc.com>
2013-10-26 00:25:52 +0300

    Added locale

---
+++ application/locale/lang_ru.php
@@ -0,0 +1,49 @@
+<?php
+return array(
+    // Ad creation form
+    'main' => "Основные",
+    'dates' => "Даты",
+    'settings' => "Настройки",
+    'contacts' => "Контакты",
+    'media' => "Изображения и видео",
+
+    'name' => 'Название',
+    'description' => 'Описание',
+    'public_date' => 'Дата публикации',
+    'start_date' => 'Дата начала',
+    'end_date' => 'Дата окончания',
+    'country' => 'Страна',
+    'region' => 'Область',
+    'city' => 'Населенный пункт',
+    'category' => 'Категория',
+    'brand' => 'Бренд',
+    'address' => 'Адресс',
+    'phone' => 'Телефон',
+    'url' => 'Сайт',
+    'image' => 'Изображение',
+    'banner' => 'Баннер',
+    'video' => 'Код видео',
+
+    // Partner profile form
+    "enterprise" => "Предприятие",
+    'info' => "Информация",
+    'profile' => "Профиль",
+
+    "new_partner_reg" => "Регистрация нового партнёра",
+    "enter" => "Вход",
+
+    'email' => "Электронная почта",
+    'password' => "Пароль",
+    'confirm_password' => "Подтверждение пароля",
+
+    'sign_in' => "Войти",
+    'finish' => 'Закончить',
+    'save_and_next' => 'Сохранить и далее',
+    'update' => "Обновить",
+    'set_new_password' => "Сменить пароль",
+    'register' => 'Зарегистрировать',
+
+    'error' => "Ошибка",
+
+    'wrong_email_or_password' => "Электронная почта или пароль не верны"
+);
