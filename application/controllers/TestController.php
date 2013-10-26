c5a0c3c68fa858dce48d7f12bbc7c8e50e72b2f4
Andrey Kuts <akuts@softserveinc.com>
2013-10-26 19:42:54 +0300

    test of access

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -10,19 +10,7 @@ class TestController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $grid = Bvb_Grid::factory('Table');
-        $source = new Bvb_Grid_Source_Zend_Table(new Application_Model_DbTable_Ad());
-        $grid->setSource($source);
-        $grid->getSelect()->where("status = ?", Application_Model_DbTable_Ad::STATUS_DRAFT);
-        $grid->setGridColumns(array("name", "public_dt", "start_dt", "end_dt"));
-        $grid->updateColumn('field',array('class'=>'my_css_class'));
-        $grid->updateColumn("name", array("title" => "Ad Name"));
-        $grid->setTemplateParams(array("cssClass" => array("table" => "table table-bordered table-striped")));
-        $grid->setNoFilters(true);
-        $grid->setExport(array());
-        $grid->setImagesUrl('/img/');
-        $this->view->grid = $grid;
-
+        $test = "aaa";
 	}
 }
 
