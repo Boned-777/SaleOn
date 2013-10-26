6929d7129cffb9a3db36f5b1517c7f50d9e47403
Andrey Kuts <akuts@softserveinc.com>
2013-10-26 20:25:28 +0300

    Added translations for partner navigation bar

--- application/views/helpers/NavigationBar.php
+++ application/views/helpers/NavigationBar.php
@@ -3,16 +3,18 @@ class Zend_View_Helper_NavigationBar extends Zend_View_Helper_Abstract
 {
 	public function NavigationBar ()
 	{
+        global $translate;
+
 		$auth = Zend_Auth::getInstance();
         $uri = Zend_Controller_Front::getInstance()->getRequest()->getRequestUri();
         $menuItems = array(
-                "profile" => array("link" => "/partner/profile", "caption" => "Мой профиль"),
-                "active" => array("link" => "/ad/active", "caption" => "Активные"),
-                "archive" => array("link" => "/ad/archive", "caption" => "Архив"),
-                "noactive" => array("link" => "/ad/noactive", "caption" => "Неоплаченные"),
-                "add_new" => array("link" => "/ad/new", "caption" => "Добавить акцию"),
-                "rules" => array("link" => "#", "caption" => "Правила"),
-                "exit" => array("link" => "/auth/logout", "caption" => "Выход")
+            "profile" => array("link" => "/partner/profile", "caption" => $translate->getAdapter()->translate("profile")),
+            "active" => array("link" => "/ad/active", "caption" => $translate->getAdapter()->translate("active")),
+            "archive" => array("link" => "/ad/archive", "caption" => $translate->getAdapter()->translate("archive")),
+            "noactive" => array("link" => "/ad/noactive", "caption" => $translate->getAdapter()->translate("noactive")),
+            "add_new" => array("link" => "/ad/new", "caption" => $translate->getAdapter()->translate("add_new")),
+            "rules" => array("link" => "#", "caption" => $translate->getAdapter()->translate("rules")),
+            "exit" => array("link" => "/auth/logout", "caption" => $translate->getAdapter()->translate("exit"))
         );
 
 		if ($auth->hasIdentity()) {
