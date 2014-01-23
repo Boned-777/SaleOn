c106eb023d88edcbfbcf2d4c7da23736decf4130
Andrey Kuts <akuts@softserveinc.com>
2014-01-23 17:27:38 +0200

    New changelist

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -10,36 +10,10 @@ class TestController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $a = new Application_Model_LiqPay();
-//        $src = "/var/www/waw/public/ads/1.jpg";
-//
-//        $target_width = 240;
-//        $target_height = 153;
-//        $image = new Application_Model_Image();
-//        $image->load($src);
-//        $image->smartResize($target_width, $target_height);
-//        $image->save("/var/www/waw/public/ads/2.jpg");
+        phpinfo();
 
         die();
 
 	}
-
-    public function authAction() {
-        $this->config = Zend_Registry::get('sauthConf');
-        $auth = Zend_Auth::getInstance();
-        if ($auth->hasIdentity()) {
-            $this->getResponse()->setRedirect($this->view->siteDir);
-        }
-        $adapterName = $this->getRequest()->getParam('by') ? $this->getRequest()->getParam('by') : 'google';
-        $adapterClass = 'SAuth_Adapter_' . ucfirst($adapterName);
-        $adapter = new $adapterClass($this->config[$adapterName]);
-        $result  = $auth->authenticate($adapter);
-        if ($result->isValid()) {
-            $this->_helper->redirector('index', 'index');
-        } else {
-            $this->view->auth = false;
-            $this->view->errors = $result->getMessages();
-        }
-    }
 }
 
