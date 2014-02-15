27a1fea6b3d07ecff54f1da8f59d97279c873225
andrey <akgalsss@gmail.com>
2014-02-15 15:13:18 +0200

    auth

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -10,30 +10,18 @@ class TestController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $mailhost= '127.0.0.1';
-		$mailconfig = array(
-			'port'    =>  '25',
-		);
+//        echo '
+//        ';
+//
+//        $consumer = new Zend_Oauth_Consumer($aGoogleConfig);
+//        $token = null;
+//
+//        die();
 
-		$transport = new Zend_Mail_Transport_Smtp ($mailhost, $mailconfig);
-		Zend_Mail::setDefaultTransport($transport);
-
-		$text = "We have created an account for you in the.\nLog in using the details below:\n\n".
-		"Email: " . "username" . "\n".
-		"Password: " . "real_password";
-
-		//echo "<h1>This is temporary solution!</h1> Message is: <br/>" . $text; exit;
-
-		$mail = new Zend_Mail();
-		$mail->setBodyText($text);
-		$mail->setFrom('admin@test.com', 'test');
-		$mail->addTo("akgals@mail.ru", '');
-		$mail->setSubject('New account for you');
-		$mail->send();
 	}
-	public function favoritesAction()
+	public function authAction()
     {
-        // action body
+        Zend_Debug::dump($this->getRequest()); die();
     }
 }
 
