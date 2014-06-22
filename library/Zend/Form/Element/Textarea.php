4d9926bf8658911c948db985f05c0823694792e0
stirlitz-ua <pokhrem@yahoo.com>
2014-06-22 18:12:22 +0300

    ...

--- library/Zend/Form/Element/Textarea.php
+++ library/Zend/Form/Element/Textarea.php
@@ -40,21 +40,3 @@ class Zend_Form_Element_Textarea extends Zend_Form_Element_Xhtml
      */
     public $helper = 'formTextarea';
 }
-
-class CK_Form_Element_CKEditor extends Zend_Form_Element_Textarea{
-
-    public function __construct($spec, $options = null)
-    {
-        parent::__construct($spec, $options);
-        //grab a reference to the view rendering the form element
-        $view = $this->getView();
-        //include scripts and initialize the ckeditor
-        $view->headScript()->appendFile(
-            '/js/ckeditor/ckeditor.js',
-            'text/javascript'
-        );
-        //give the textarea a class name that ckeditor recognises
-        $this->setAttrib('class', 'ckeditor');
-    }
-}
-?>
