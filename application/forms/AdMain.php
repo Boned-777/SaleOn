adcb646dee412c5eafcaa5769ccd510c78e66891
stirlitz-ua <pokhrem@yahoo.com>
2014-06-22 17:06:08 +0300

    Added ckEditor for better text editing and formating

--- application/forms/AdMain.php
+++ application/forms/AdMain.php
@@ -30,8 +30,8 @@ class Application_Form_AdMain extends Zend_Form
             ),
             'required' => true,
         ));
-
-        $this->addElement('textarea', 'description', array(
+        $this->
+        $this->setupEditor('textarea', 'description', array(
             'class' => "input-block-level",
             'label' => $translate->getAdapter()->translate("description") . ' *',
             'max_length' => 250,
@@ -50,7 +50,7 @@ class Application_Form_AdMain extends Zend_Form
             'required' => true,
         ));
 
-        $this->addElement('textarea', 'full_description', array(
+        $this->setupEditor('textarea', 'full_description', array(
             'class' => "input-block-level",
             'label' => $translate->getAdapter()->translate("full_description"),
             'max_length' => 10000,
---
+++ application/views/helpers/SetupEditor.php
@@ -0,0 +1,18 @@
+<?php
+class Zend_View_Helper_SetupEditor {
+
+    function setupEditor( $textareaId ) {
+        return "<script type=\"text/javascript\">
+ CKEDITOR.replace( '". $textareaId ."' );
+  </script>";
+    }
+}
+?>
+<?php
+class Zend_View_Helper_BaseUrl {
+    function baseUrl() {
+        $fc = Zend_Controller_Front::getInstance();
+        return $fc->getBaseUrl();
+    }
+}
+?>
