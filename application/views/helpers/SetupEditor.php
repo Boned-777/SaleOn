19f8db877058121dcf8d837d32b41b15b035ffee
stirlitz-ua <pokhrem@yahoo.com>
2014-06-23 10:03:34 +0300

    CKeditor fix

--- application/views/helpers/SetupEditor.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Zend_View_Helper_CKEditor {
-    function CKEditor( $textareaId ) {
-        return "<script type=\"text/javascript\">
-                       CKEDITOR.replace( '". $textareaId ."' );
-                  </script>";
-    }
-}
-?>
