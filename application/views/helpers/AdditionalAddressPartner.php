5a5bfc3fee880d95cb54135c74c54de53b767b46
stirlitz-ua <pokhrem@yahoo.com>
2014-07-24 09:07:27 +0300

    Some more UI fixes to partner view

--- application/views/helpers/AdditionalAddressPartner.php
+++ application/views/helpers/AdditionalAddressPartner.php
@@ -16,7 +16,7 @@ class Zend_View_Helper_AdditionalAddressPartner extends Zend_View_Helper_Abstrac
             echo '</ul>';
             echo '</div>';
 
-            echo '<div class="input-append" style="margin-left:18px; margin-top: 10px;">';
+            echo '<div class="input-append" style="margin-left:18px;">';
                 echo '<input id="new_address" class="form-control" type="text" style="width: 330px;"/>';
                 echo '<span class="add_address add-on">' . $translate->getAdapter()->translate("add") . '</span>';
             echo '</div>';
