9398a2391b0101e727ed7d0ad8018f3e5f8c8994
stirlitz-ua <pokhrem@yahoo.com>
2014-08-12 17:18:39 +0300

    Adding missing index controller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -30,7 +30,36 @@ class IndexController extends Zend_Controller_Action
 
     public function contactsAction()
     {
-        // action body
+        {
+// action body
+// Create form instance
+            $form = new Application_Form_Contact();
+
+            /**
+             * Get request
+             */
+            $request = $this->getRequest();
+            $post = $request->getPost(); // This contains the POST params
+
+            /**
+             * Check if form was sent
+             */
+            if ($request->isPost()) {
+                /**
+                 * Check if form is valid
+                 */
+                if ($form->isValid($post)) {
+// build message
+                    $message = 'From: ' . $post['name'] . chr(10) . 'Email: ' . $post['email'] . chr(10) . 'Message: ' . $post['message'];
+// send mail
+                    mail('boned@ukr.net', 'contact: ' . $post['subject'], $message);
+                }
+            }
+
+// give form to view (needed in index.phtml file)
+            $this->view->form = $form;
+
+        }
     }
 
 }
