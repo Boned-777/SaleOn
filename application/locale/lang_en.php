0ab26cfdd4ad255125804e9a60745b6162891af6
stirlitz-ua <pokhrem@yahoo.com>
2014-09-10 23:38:02 +0300

    uploading latest changes

--- application/locale/lang_en.php
+++ application/locale/lang_en.php
@@ -157,7 +157,7 @@ return array (
     "mm_favorites" => "Favorites",
     "mm_user" => "User",
 
-    "main_title" => "All Special offers",
+    "main_title" => "All Special Offers",
     "copyright" => "© All Specials of Ukraine, 2014",
     "lng_russian" => "Русский",
     "lng_ukrainian" => "Українська",
