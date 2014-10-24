b4bfccf1730df7c8d49e995fe367fe7940a697b8
andrey <akgalsss@gmail.com>
2014-10-24 20:45:00 +0300

    Added add item

---
+++ application/views/helpers/AdItem.php
@@ -0,0 +1,17 @@
+<?php
+class Zend_View_Helper_AdItem extends Zend_View_Helper_Abstract
+{
+	public function AdItem ($data)
+    {
+?>
+        <a class="img-wrapper" href="/ad/index/id/<?= $data["post_id"] ?>"> <img src="/media/<?= $data["photoimg"] ?>" class="img-polaroid">
+            <div class="post-link img-info">
+                <div data-link="/auth" title="Додати в обране" data-id="130" class="favorites-icon favorites-icon-off"></div>
+                <p class="ellipsis"><?= $data["name"] ?></p>
+                <p class="ellipsis"><?= $data["brand_name"] ?></p>
+                <p class="ellipsis">Залишилося днів: <?= $data["days"] ?></p>
+            </div>
+        </a>
+<?php
+	}
+}
