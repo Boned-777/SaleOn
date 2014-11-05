c74255ebdf767e54d8c5652e90b6e3a5ef47433b
Eugene <dreemius@gmail.com>
2014-11-05 10:19:53 +0200

    Update main page design. (img shadow, font)

--- application/views/helpers/AdItem.php
+++ application/views/helpers/AdItem.php
@@ -1,16 +1,18 @@
+
 <?php
 class Zend_View_Helper_AdItem extends Zend_View_Helper_Abstract
 {
-	public function AdItem ($data)
-    {
+
+	public function AdItem ($data) {
+        global $translate;
 ?>  
     <div class="span3 bottom-offset">
         <a class="img-wrapper" href="/ad/index/id/<?= $data["post_id"] ?>"> <img src="/media/<?= $data["photoimg"] ?>" class="img-polaroid">
             <div class="post-link img-info">
-                <div data-link="/auth" title="Додати в обране" data-id="130" class="favorites-icon favorites-icon-off"></div>
+                <div data-link="/auth" title="<?= $translate->getAdapter()->translate("add_to_favorites") ?>" data-id="<?= $data["post_id"] ?>" class="favorites-icon favorites-icon-off"></div>
                 <p class="ellipsis"><?= $data["name"] ?></p>
                 <p class="ellipsis"><?= $data["brand_name"] ?></p>
-                <p class="ellipsis">Залишилося днів: <?= $data["days"] ?></p>
+                <p class="ellipsis"><?= $translate->getAdapter()->translate("days_left"). ": " .$data["days"] ?></p>
             </div>
         </a>
     </div>    
