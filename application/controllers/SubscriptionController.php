2632bf074ee1499e4a0f499447aab9d0a0c57769
andrey <akgalsss@gmail.com>
2015-03-10 15:10:28 +0200

    views added

--- application/controllers/SubscriptionController.php
+++ application/controllers/SubscriptionController.php
@@ -1,25 +1,62 @@
 <?php
+
 class SubscriptionController extends Zend_Controller_Action
 {
+    var $user;
 
+    public function init()
+    {
+        $auth = Zend_Auth::getInstance();
+        if ($auth->getIdentity()->role != Application_Model_User::USER) {
+            $this->_helper->redirector('index', 'auth');
+        }
+        $this->user = $auth->getIdentity();
+    }
+
+    /**
+     *
+     */
     public function indexAction()
     {
-        $brands = new Application_Model_Brand();
-        //$brands->getAll();
+        $request = $this->getRequest();
+        $form = new Application_Form_Subscription();
 
-        Zend_Debug::dump($brands->getAll()); die();
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $item = new Application_Model_Subscription();
+                $data = array_merge(
+                    $request->getPost(),
+                    array("user_id" => $this->user->id)
+                );
+                $item->create($data);
+            }
+        }
 
-        $this->view->brands = $brands;
+        $this->view->subscriptionForm = $form;
     }
 
-    public function addAction()
-    {
+    public function managerAction () {
+        $request = $this->getRequest();
+        $item = new Application_Model_Subscription();
 
+        if ($this->getRequest()->isPost()) {
+            $formData = $request->getPost();
+
+            $subscriptionItems = $item->getByUserId($this->user->id, true);
+            foreach ($subscriptionItems as $subscriptionItem) {
+                if (!isset($formData["brand"][$subscriptionItem->brand_id])) {
+                    $subscriptionItem->delete();
+                }
+            }
+        }
+
+        $subscriptionItems = $item->getByUserId($this->user->id);
+        $this->view->items = $subscriptionItems;
     }
 
-    public function removeAction()
-    {
-
+    public function sendAuction() {
+        $subscription = new Application_Model_Subscription();
+        $subscription->send();
     }
 
 }
---
+++ application/forms/Subscription.php
@@ -0,0 +1,68 @@
+<?php
+
+class Application_Form_Subscription extends Zend_Form
+{
+
+    public function init()
+    {
+        global $translate;
+
+        $scriptElement = new Custom_Form_Element_Universal("script");
+        $scriptElement->setValue("
+            <script>
+            $( document ).ready(function() {
+                $('#description-label').hide();
+                $('#description-element').hide();
+            });
+            </script>
+        ");
+        $scriptElement->removeDecorator("Label");
+
+        $this->addElement($scriptElement);
+
+        $this->addElement('hidden', 'brand_id');
+        $this->getElement("brand_id")->setDecorators(array('ViewHelper'));
+
+        $brand = new ZendX_JQuery_Form_Element_AutoComplete('brand_name', array(
+            'class' => "input-block-level",
+        ));
+        $brand->setLabel($translate->getAdapter()->translate("subscription_brand"));
+        $brand->setJQueryParam('source', '/brands/autocomp');
+        $brand->setJQueryParam('response', new Zend_Json_Expr(
+            'function (e, data) {
+                $("#brand_id").val("");
+                if (data.content.length) {
+                    $("#description-label").hide();
+                    $("#description-element").hide();
+                } else {
+                    $("#description-label").show();
+                    $("#description-element").show();
+                }
+                return false;
+            }'));
+        $brand->setJQueryParam('select', new Zend_Json_Expr(
+            'function (e, data) {
+                $("#brand_name").val(data.item.label);
+                $("#brand_id").val(data.item.value);
+                return false;
+            }'));
+        $this->addElement($brand);
+
+        $this->addElement('textarea', 'subscription_description', array(
+            'class' => "input-block-level hide",
+            'label' => $translate->getAdapter()->translate("details"),
+            'validators' => array(
+                array('StringLength', false, array(0, 500))
+            ),
+            //'required' => true,
+        ));
+
+        $this->addElement('submit', "subscription_submit", array(
+            //'class' => 'btn btn-large btn-primary',
+            'required' => false,
+            'ignore' => true,
+            'label' => $translate->getAdapter()->translate("update"),
+        ));
+    }
+
+}
