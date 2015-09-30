8c1ae1e26948a6382a3780ce1e0b1dad46b72acc
andrey <akgalsss@gmail.com>
2015-09-30 23:39:00 +0300

    added ad details feed

---
+++ application/views/helpers/MainMenu.php
@@ -0,0 +1,18 @@
+<?php
+class Zend_View_Helper_MainMenu extends Zend_View_Helper_Abstract
+{
+	public function MainMenu ()
+	{
+		$auth = Zend_Auth::getInstance();
+        $view = new Zend_View();
+        $view->setScriptPath(APPLICATION_PATH . "/views/helpers/views");
+
+        $params = array();
+		if ($auth->hasIdentity()) {
+            if (isset($auth->getIdentity()->role) && $auth->getIdentity()->role === Application_Model_User::PARTNER){
+                $params["username"] = $auth->getIdentity()->username;
+            }
+        }
+        echo $view->partial("mainMenu.phtml", $params);
+	}
+}
