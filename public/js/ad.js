7b1682bf3f5aea9964e64f5103170636b78040f7
andrey <akgalsss@gmail.com>
2015-10-07 23:54:16 +0300

    fixed partner

--- public/js/ad.js
+++ public/js/ad.js
@@ -62,9 +62,9 @@ $(function () {
     function initDatapickers () {
         if(datesTabLoaded) return;
         var lng = $('#current-lng').val();
-//        $('#start_dt').datepicker({format: 'yyyy-mm-dd', weekStart: 1, lng: lng});
-        $('#end_dt').datepicker({format: 'yyyy-mm-dd', weekStart: 1, lng: lng});
-        $('#public_dt').datepicker({format: 'yyyy-mm-dd', weekStart: 1, lng: lng});   
+//        $('#start_dt').datepicker({format: 'yy-mm-dd', weekStart: 1, lng: lng});
+        $('#end_dt').datepicker({dateFormat: 'yy-mm-dd', weekStart: 1, lng: lng});
+        $('#public_dt').datepicker({dateFormat: 'yy-mm-dd', weekStart: 1, lng: lng});
         datesTabLoaded = true; 
     }
     
